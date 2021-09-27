478fa6a4fd2020ab6d56ddd22ae9ce1c259f6ce9
Viktoria <vikakabishova@outlook.com>
2021-09-27 18:56:03 +0300

    first commit

---
+++ Numbers5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Numbers5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i++ <= 100)
+            {
+                if (i % 3 != 0 && i % 5 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    if (i % 3 == 0)
+                        Console.WriteLine("Ouu");
+                    if (i % 5 == 0)
+                        Console.WriteLine("May");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
