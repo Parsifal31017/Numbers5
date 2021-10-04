87fea9f47cd096e3a1cb31c93502de635401b7f6
Viktoria <vikakabishova@outlook.com>
2021-10-04 11:14:47 +0300

    first commit

--- Numbers5/Program.cs
+++ Numbers5/Program.cs
@@ -6,17 +6,25 @@ namespace Numbers5
     {
         static void Main()
         {
+            Console.Write("Введите текст: ");
+            string text1 = Console.ReadLine();
+            Console.Write("Введите число: ");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите текст: ");
+            string text2 = Console.ReadLine();
+            Console.Write("Введите число: ");
+            int number2 = int.Parse(Console.ReadLine());
             for (var i = 1; i <= 100; i++)
             {
-                var dividesBy3 = i % 3 == 0;
-                var dividesBy5 = i % 5 == 0;
+                var dividesBy3 = i % number1 == 0;
+                var dividesBy5 = i % number2 == 0;
 
                 if (dividesBy3 && dividesBy5)
-                    Console.WriteLine("OuuMay");
+                    Console.WriteLine(text1 + text2);
                 else if (dividesBy3)
-                    Console.WriteLine("Ouu");
+                    Console.WriteLine(text1);
                 else if (dividesBy5)
-                    Console.WriteLine("May");
+                    Console.WriteLine(text2);
                 else
                     Console.WriteLine(i);
             }
